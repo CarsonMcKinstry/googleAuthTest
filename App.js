7ac99b4499375198cfafb4799e4595f36a39c9ab
AndreiCalazans <andreixoc@hotmail.com>
2018-06-20 20:08:40 -0300

    add readme

--- App.js
+++ App.js
@@ -1,40 +1,102 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, Text, View, TouchableOpacity, Platform } from 'react-native'
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin'
+import config from './config'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: null
+    }
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  componentDidMount() {
+    this._setupGoogleSignin()
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    if (!this.state.user) {
+      return (
+        <View style={styles.container}>
+          <GoogleSigninButton
+            style={{ width: 212, height: 48 }}
+            size={GoogleSigninButton.Size.Standard}
+            color={GoogleSigninButton.Color.Auto}
+            onPress={this._signIn.bind(this)}
+          />
+        </View>
+      )
+    }
+
+    if (this.state.user) {
+      return (
+        <View style={styles.container}>
+          <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 20 }}>
+            Welcome {this.state.user.name}
+          </Text>
+          <Text>Your email is: {this.state.user.email}</Text>
+
+          <TouchableOpacity
+            onPress={() => {
+              this._signOut()
+            }}
+          >
+            <View style={{ marginTop: 50 }}>
+              <Text>Log out</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      )
+    }
+  }
+
+  async _setupGoogleSignin() {
+    try {
+      await GoogleSignin.hasPlayServices({ autoResolve: true })
+      const configPlatform = {
+        ...Platform.select({
+          ios: {
+            iosClientId: config.iosClientId
+          },
+          android: {}
+        })
+      }
+
+      await GoogleSignin.configure({
+        ...configPlatform,
+        webClientId: config.webClientId,
+        offlineAccess: false
+      })
+
+      const user = await GoogleSignin.currentUserAsync()
+      console.log(user)
+      this.setState({ user })
+    } catch (err) {
+      console.warn('Google signin error', err.code, err.message)
+    }
+  }
+
+  _signIn() {
+    GoogleSignin.signIn()
+      .then(user => {
+        console.log(user)
+        this.setState({ user: user })
+      })
+      .catch(err => {
+        console.warn(err)
+      })
+      .done()
+  }
+
+  _signOut() {
+    GoogleSignin.revokeAccess()
+      .then(() => GoogleSignin.signOut())
+      .then(() => {
+        this.setState({ user: null })
+      })
+      .done()
   }
 }
 
@@ -43,16 +105,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
+
+export default App;
---
+++ README.md
@@ -0,0 +1,14 @@
+## Google implementation example.
+
+*Simple google auth example using react-native-google-sign.*
+
+
+### Install
+
+1) yarn install
+2) react-native run-[ios || android]
+
+
+
+- [x] Example with IOS.
+- [] Example with Android.
